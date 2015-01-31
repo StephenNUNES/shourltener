ffc0a08bb53fcb6b3e2beb749cafc7b4348f2a13
Stephen <nunes.stephen.13@gmail.com>
2015-01-31 16:15:28 +0100

    Configuration du projet avec ajout du engine template Nunjucks

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html', { title: 'Express' });
 });
 
+//router.get('/', function(request, response) {
+ //  res.render('index.html');
+//});
 module.exports = router;
